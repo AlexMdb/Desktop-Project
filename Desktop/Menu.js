0eb03b96e34d0d370aa1932940e031269975b236
Aurora Santos <alexanders4nt0splus@gmail.com>
2024-02-02 17:18:08 -0300

    Added an Options menu, currently working on the ability to change the wallpaper

---
+++ Desktop/Menu.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById('options-menu');
+const kebab = document.getElementById('kebab');
+
+function showMenu(){
+    menu.style.left = '0vw';
+    kebab.style.display = 'none';
+}
+
+function hideMenu(){
+    menu.style.left = '-40vw';
+    kebab.style.display = 'block';
+}
+
+document.addEventListener('click', function(event){
+    if (event.target !== menu && event.target !== kebab){
+        hideMenu();
+    }
+});
