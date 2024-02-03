908fd48cb8ccb8616422dccc08f9daaa151abb17
EtherAurora <alexanders4nt0splus@gmail.com>
2024-02-03 08:51:28 -0300

    Made the loading screen better.

--- Desktop/loading.js
+++ Desktop/loading.js
@@ -1,5 +1,7 @@
 window.onload = function () {
-        document.getElementById('loading-screen').style.display = 'none';
+        var loadingScreen = document.getElementById('loading');
+
+        loadingScreen.style.display = 'none'
 
         // Show the main content
         document.getElementById('main').style.display = 'block';
